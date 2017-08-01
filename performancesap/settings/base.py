e3b585600a8913346b258e3ae9893965a21d40db
Rafael Basile <rafaelbasile@gmail.com>
2017-07-31 23:22:07 -0300

    allowed heroku

--- performancesap/settings/base.py
+++ performancesap/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='PleaseChangeThisKey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','locahost','performanceap.com','www.performancesap.com']
+ALLOWED_HOSTS = ['127.0.0.1','locahost','performanceap.com','www.performancesap.com','performancesap.herokuapp.com']
 
 
 # Application definition
